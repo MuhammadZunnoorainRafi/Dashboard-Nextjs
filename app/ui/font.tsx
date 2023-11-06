5bdd1d36ceb5ec80c163aebab29d8e1f59ad65e5
Muhammad Zunnoorain Rafi <bhoshyl@gmail.com>
2023-11-06 11:46:49 +0500

    Navigating between pages completed

--- app/ui/font.tsx
+++ app/ui/font.tsx
@@ -1,5 +1,9 @@
 import { Lusitana, Inter } from 'next/font/google';
 
+export const inter = Inter({
+  subsets: ['latin'],
+  weight: ['200', '400', '900', '700'],
+});
 export const lusitana = Lusitana({
   subsets: ['latin'],
   weight: ['400', '700'],
